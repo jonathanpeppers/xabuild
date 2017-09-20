bf38811e2205c2b7fd951e77a1ed3be082785386
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-09-20 08:20:27 -0500

    Move SymbolicLink code to its own class
    Seems to keep XABuild.cs a bit cleaner

---
+++ Xamarin.Android.Build/SymbolicLink.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+
+#if UNIX
+using Mono.Unix;
+#endif
+
+namespace Xamarin.Android.Build
+{
+	static class SymbolicLink
+	{
+		public static bool Create (string source, string target)
+		{
+			if (!Directory.Exists (source)) {
+#if UNIX
+				var fileInfo = new UnixFileInfo (target);
+				var link = fileInfo.CreateSymbolicLink (source);
+				if (!link.Exists) {
+					Console.Error.WriteLine ($"Unable to create symbolic link from `{source}` to `{target}`");
+					return false;
+				}
+#else
+				//NOTE: attempt with and without the AllowUnprivilegedCreate flag, seems to fix Windows Server 2016
+				if (!CreateSymbolicLink (source, target, SymbolLinkFlag.Directory | SymbolLinkFlag.AllowUnprivilegedCreate) &&
+					!CreateSymbolicLink (source, target, SymbolLinkFlag.Directory)) {
+					var error = new Win32Exception ().Message;
+					Console.Error.WriteLine ($"Unable to create symbolic link from `{source}` to `{target}`: {error}");
+					return false;
+				}
+#endif
+			}
+
+			return true;
+		}
+
+#if !UNIX
+		enum SymbolLinkFlag {
+			File = 0,
+			Directory = 1,
+			AllowUnprivilegedCreate = 2,
+		}
+
+		[DllImport ("kernel32.dll")]
+		[return: MarshalAs (UnmanagedType.I1)]
+		static extern bool CreateSymbolicLink (string lpSymlinkFileName, string lpTargetFileName, SymbolLinkFlag dwFlags);
+#endif
+	}
+}
--- Xamarin.Android.Build/XABuild.cs
+++ Xamarin.Android.Build/XABuild.cs
@@ -1,22 +1,10 @@
 ﻿using Microsoft.Build.CommandLine;
 using System;
-using System.ComponentModel;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Xml;
-
-#if UNIX
-using Mono.Unix;
-#endif
 
 namespace Xamarin.Android.Build
 {
-	enum SymbolLinkFlag {
-		File = 0,
-		Directory = 1,
-		AllowUnprivilegedCreate = 2,
-	}
-
 	class XABuild
 	{
 		[MTAThread]
@@ -32,7 +20,7 @@ namespace Xamarin.Android.Build
 			CreateConfig (paths);
 
 			//Create link to .NETPortable directory
-			if (!CreateSymbolicLink (Path.Combine (paths.FrameworksDirectory, ".NETPortable"), paths.PortableProfiles)) {
+			if (!SymbolicLink.Create (Path.Combine (paths.FrameworksDirectory, ".NETPortable"), paths.PortableProfiles)) {
 				return 1;
 			}
 
@@ -82,33 +70,5 @@ namespace Xamarin.Android.Build
 				toolsets.PrependChild (property);
 			}
 		}
-
-		static bool CreateSymbolicLink (string source, string target)
-		{
-			if (!Directory.Exists (source)) {
-#if UNIX
-				var fileInfo = new UnixFileInfo (target);
-				var link = fileInfo.CreateSymbolicLink (source);
-				if (!link.Exists) {
-					Console.Error.WriteLine ($"Unable to create symbolic link from `{source}` to `{target}`");
-					return false;
-				}
-#else
-				//NOTE: attempt with and without the AllowUnprivilegedCreate flag, seems to fix Windows Server 2016
-				if (!CreateSymbolicLink (source, target, SymbolLinkFlag.Directory | SymbolLinkFlag.AllowUnprivilegedCreate) &&
-					!CreateSymbolicLink (source, target, SymbolLinkFlag.Directory)) {
-					var error = new Win32Exception ().Message;
-					Console.Error.WriteLine ($"Unable to create symbolic link from `{source}` to `{target}`: {error}");
-					return false;
-				}
-#endif
-			}
-
-			return true;
-		}
-
-		[DllImport ("kernel32.dll")]
-		[return: MarshalAs (UnmanagedType.I1)]
-		static extern bool CreateSymbolicLink (string lpSymlinkFileName, string lpTargetFileName, SymbolLinkFlag dwFlags);
 	}
 }
