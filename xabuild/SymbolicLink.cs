ccf2241328f60d1a34ddd85c3d2434f24dc35779
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-09-20 13:22:28 -0500

    Removed all preprocessor stuff

--- xabuild/SymbolicLink.cs
+++ xabuild/SymbolicLink.cs
@@ -2,10 +2,7 @@
 using System.ComponentModel;
 using System.IO;
 using System.Runtime.InteropServices;
-
-#if UNIX
 using Mono.Unix;
-#endif
 
 namespace Xamarin.Android.Build
 {
@@ -14,28 +11,27 @@ namespace Xamarin.Android.Build
 		public static bool Create (string source, string target)
 		{
 			if (!Directory.Exists (source)) {
-#if UNIX
-				var fileInfo = new UnixFileInfo (target);
-				var link = fileInfo.CreateSymbolicLink (source);
-				if (!link.Exists) {
-					Console.Error.WriteLine ($"Unable to create symbolic link from `{source}` to `{target}`");
-					return false;
+				if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
+					//NOTE: attempt with and without the AllowUnprivilegedCreate flag, seems to fix Windows Server 2016
+					if (!CreateSymbolicLink (source, target, SymbolLinkFlag.Directory | SymbolLinkFlag.AllowUnprivilegedCreate) &&
+						!CreateSymbolicLink (source, target, SymbolLinkFlag.Directory)) {
+						var error = new Win32Exception ().Message;
+						Console.Error.WriteLine ($"Unable to create symbolic link from `{source}` to `{target}`: {error}");
+						return false;
+					}
+				} else {
+					var fileInfo = new UnixFileInfo (target);
+					var link = fileInfo.CreateSymbolicLink (source);
+					if (!link.Exists) {
+						Console.Error.WriteLine ($"Unable to create symbolic link from `{source}` to `{target}`");
+						return false;
+					}
 				}
-#else
-				//NOTE: attempt with and without the AllowUnprivilegedCreate flag, seems to fix Windows Server 2016
-				if (!CreateSymbolicLink (source, target, SymbolLinkFlag.Directory | SymbolLinkFlag.AllowUnprivilegedCreate) &&
-					!CreateSymbolicLink (source, target, SymbolLinkFlag.Directory)) {
-					var error = new Win32Exception ().Message;
-					Console.Error.WriteLine ($"Unable to create symbolic link from `{source}` to `{target}`: {error}");
-					return false;
-				}
-#endif
 			}
 
 			return true;
 		}
 
-#if !UNIX
 		enum SymbolLinkFlag {
 			File = 0,
 			Directory = 1,
@@ -45,6 +41,5 @@ namespace Xamarin.Android.Build
 		[DllImport ("kernel32.dll")]
 		[return: MarshalAs (UnmanagedType.I1)]
 		static extern bool CreateSymbolicLink (string lpSymlinkFileName, string lpTargetFileName, SymbolLinkFlag dwFlags);
-#endif
 	}
 }
